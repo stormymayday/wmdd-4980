92b5983824fb4570c594593dd73f6c91f86c9674
Leo <repiklleonid@gmail.com>
2024-03-04 20:16:29 -0800

    Pie chart update and some css fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+// import App from './App.jsx';
 import './index.css';
 
+import { CrewAvailability } from './components/CrewPieChart/CrewAvailability.jsx';
+import '../SASS/main.scss';
 // fetch('/api/v1/users/65c00e3cfd44bd16f80d5131')
 //   .then((res) => res.json())
 //   .then((data) => console.log(data));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CrewAvailability />
   </React.StrictMode>
 );
