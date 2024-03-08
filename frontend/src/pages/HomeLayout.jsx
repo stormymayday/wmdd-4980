8876de844285fbeef6583433296ca9468a9e5d09
Victorportus <vic.portus@gmail.com>
2024-03-07 21:53:02 -0800

    Add crew component created and functionality working on the flow

--- frontend/src/pages/HomeLayout.jsx
+++ frontend/src/pages/HomeLayout.jsx
@@ -1,16 +1,8 @@
 import { Outlet } from 'react-router-dom';
-import FlightTable from '../components/flightTable/FlightTable'
-import FlightTableWithCrew from '../components/flightTableWithCrew/FlightTableWithCrew'
-import BarChart from '../components/BarChart/BarChart'
-import { CrewAvailability } from '../components/CrewPieChart/CrewAvailability';
 const HomeLayout = () => {
   return (
     <div>
-      <BarChart />
-      {/* <FlightTable /> */}
-      <FlightTableWithCrew expand={true} />
-      {/* <CrewAvailability/> */}
-      {/* <Outlet /> */}
+      <Outlet />
     </div>
   );
 };
